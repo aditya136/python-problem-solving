0557c4143c9789db400dcf9ed13e53d76b365a08
aditya136 <adityabanerjee102@gmail.com>
2023-08-04 22:30:22 +0600

    math-problem-solving-and-recursion-code

---
+++ math1-prob.py
@@ -0,0 +1,6 @@
+#Power's problem. 
+i = 1
+while i < 50:
+    s = (2 ** i)
+    i += 1
+print(s)
---
+++ recursion.py
@@ -0,0 +1,10 @@
+#Recursion
+def recursion(n):
+  if(n>4):
+    result = 6 + recursion(n - 1)
+    print(result)
+  else:
+    result = 0
+  return result
+
+recursion(6)
