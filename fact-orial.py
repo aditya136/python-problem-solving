df25cd52f8ea829dc5c2b9301c714e40d68538ad
aditya136 <adityabanerjee102@gmail.com>
2023-08-05 15:43:32 +0600

    factorial-in-Python

--- fact-orial.py
+++ fact-orial.py
@@ -1,11 +1,32 @@
 #Factorail code
 
+
+#Using function
 def factorial(n):
     if n == 0 or n == 1:
         return 1
     else:
         return n * factorial(n-1)
     
+number = int(input("Factorial Number: "))
+n = number
+print(factorial(n))
+
+
+
+
+#We can also do it with the help of for loop
+def factorial(n):
+    if n < 0:
+        raise ValueError("Factorial is not defined for negative numbers.")
+    
+    result = 1
+    for i in range(1, n + 1):
+        result *= i
+    
+    return result
+
+
 number = int(input("Factorial Number: "))
 n = number
 print(factorial(n))
