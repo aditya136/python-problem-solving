3d1fdaceb890238c49f3ebe1f09a49b02a4c21c9
aditya136 <adityabanerjee102@gmail.com>
2023-08-07 23:38:30 +0600

    How-to-reverse-a-number-without-using-the-reverse-function-in-Python

--- reverse.py
+++ reverse.py
@@ -1,7 +1,23 @@
 #Problem-  I have to reverse a number/word without using the built in reverse method
 #Let's solve the problem-
 
-li = [1,3,2]
-print(li[2])
-print(li[1])
-print(li[0])
+x = input("Enter the number you want to reverse: ")
+n = int(x)
+s = 0
+while n:         #if n == 115
+    r = n % 10    #5
+    n //= 10    #11
+    s = s + r
+    print(r)
+
+
+
+#An another way
+x = input("Enter the number you want to reverse: ")
+n = int(x)
+s = 0
+while n:         #if n == 115
+    r =n % 10  #5
+    s = s * 10 + r  #5
+    n //= 10 #11
+    print(r)
