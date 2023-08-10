8fb282205cdf591981211816e36af4dc2b950b41
aditya136 <adityabanerjee102@gmail.com>
2023-08-10 10:45:07 +0600

    print-the-all-odd-&-even-num-form:1-to:100-in-Python

---
+++ num-printing.py
@@ -0,0 +1,15 @@
+#There is a problem like we have to print the odd numbers from 1 to 100 like this-  2,4,6....
+
+#Now let's solve the problem
+for i in range(1, 100, 2):
+    i += 1
+    print(i)
+
+
+
+
+#There is a problem like we have to print the even numbers from 1 to 100 like this-  2,4,6....
+
+#Solution-
+for i in range(1, 100, 2):
+    print(i)
